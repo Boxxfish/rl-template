a1976a6f28d8a29150eb184a9b59fc8584c2a7ff
Ben Giacalone <bensgiacalone@gmail.com>
2025-01-03 17:24:13 -0500

    Update experiment utils

--- rl_template/utils.py
+++ rl_template/utils.py
@@ -1,9 +1,56 @@
 import importlib
 from typing import Tuple
+from pydantic import BaseModel
 
 import torch
 from torch import nn
 
+T = TypeVar("T", bound=BaseModel)
+
+
+def parse_args(cfg_t: type[T]) -> T:
+    parser = ArgumentParser()
+    for k, v in cfg_t.model_fields.items():
+        flag_name = f"--{k.replace('_', '-')}"
+        if v.annotation == bool:
+            parser.add_argument(flag_name, default=v.default, action="store_true")
+        elif get_origin(v.annotation) == Literal:
+            choices = list(get_args(v.annotation))
+            parser.add_argument(
+                flag_name, default=v.default, choices=choices, type=type(choices[0])
+            )
+        else:
+            assert v.annotation is not None
+            parser.add_argument(flag_name, default=v.default, type=v.annotation)
+    args = parser.parse_args()
+    cfg = cfg_t(**args.__dict__)
+    return cfg
+
+
+def create_directory(out_dir_: str, meta: T) -> Path:
+    for _ in range(100):
+        if wandb.run.name not in ["" or None]:
+            break
+    if wandb.run.name not in ["" or None]:
+        out_id = wandb.run.name
+    else:
+        out_id = "testing"
+
+    out_dir = Path(out_dir_)
+    exp_dir = out_dir / out_id
+    try:
+        os.mkdir(exp_dir)
+    except OSError as e:
+        print(e)
+    with open(exp_dir / "meta.json", "w") as f:
+        f.write(meta.model_dump_json(indent=2))
+
+    chkpt_path = exp_dir / "checkpoints"
+    try:
+        os.mkdir(chkpt_path)
+    except OSError as e:
+        print(e)
+    return chkpt_path
 
 def get_img_size(old_h: int, old_w: int, conv: torch.nn.Conv2d) -> Tuple[int, int]:
     """
